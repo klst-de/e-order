52ead7f021d93468b1d654d3f3fd249a6a477e64
homebeaver <homebeaver@outlook.de>
2021-02-17 00:27:57 +0100

    class ID extends IDType implements Identifier

---
+++ src/main/java/com/klst/eorder/impl/ID.java
@@ -0,0 +1,58 @@
+package com.klst.eorder.impl;
+
+import com.klst.eorder.Identifier;
+
+import un.unece.uncefact.data.standard.unqualifieddatatype._103.IDType;
+
+/*
+ 
+Component                 | Use         	Primitive Type | Example
+Content                   : Mandatory 		String         | abc:123-DEF
+Scheme identifier         : Conditional 	String         | GLN
+Scheme version identifier : Conditional 	String         | 1.0          // nicht in unqualifieddatatype._103
+ 
+ */
+public class ID extends IDType implements Identifier /*, Reference*/ {
+
+	public ID(String content, String schemeID) {
+		super();
+		setContent(content);
+		setSchemeIdentifier(schemeID);
+	}
+
+	public ID(String content) {
+		this(content, null);
+	}
+		
+	// copy ctor
+	public ID(IDType id) {
+		this(id.getValue(), id.getSchemeID());
+	}
+	
+	public String toString() {
+		return "["+(super.getSchemeID()==null? "":",SchemeID="+getSchemeID())
+			+"\""+super.getValue()+"\"]";
+	}
+
+	@Override
+	public void setContent(String content) {
+		super.setValue(content);
+	}
+
+	@Override
+	public String getContent() {
+		return super.getValue();
+	}
+
+	@Override
+	public void setSchemeIdentifier(String schemeId) {
+		if(schemeId==null) return;
+		super.setSchemeID(schemeId);
+	}
+
+	@Override
+	public String getSchemeIdentifier() {
+		return super.getSchemeID();
+	}
+
+}
