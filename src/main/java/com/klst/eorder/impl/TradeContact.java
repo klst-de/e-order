3c6a39b38d54eeccb2694cd3722e7872114b5004
homebeaver <homebeaver@outlook.de>
2021-02-19 22:34:38 +0100

    TradeContact extends TradeContactType implements IContact

---
+++ src/main/java/com/klst/eorder/impl/TradeContact.java
@@ -0,0 +1,98 @@
+package com.klst.eorder.impl;
+
+import com.klst.einvoice.reflection.CopyCtor;
+import com.klst.eorder.IContact;
+
+import un.unece.uncefact.data.standard.reusableaggregatebusinessinformationentity._103.TradeContactType;
+import un.unece.uncefact.data.standard.reusableaggregatebusinessinformationentity._103.UniversalCommunicationType;
+
+public class TradeContact extends TradeContactType implements IContact {
+
+	@Override // implements IContactFactory
+	public IContact createContact(String contactName, String contactTel, String contactMail) {
+		return create(contactName, contactTel, contactMail);
+	}
+	
+	static TradeContact create(String contactName, String contactTel, String contactMail) {
+		return new TradeContact(contactName, contactTel, contactMail);
+	}
+	
+	public static TradeContact create() {
+		return create((TradeContactType)null);
+	}
+	// copy factory
+	static TradeContact create(TradeContactType object) {
+		// @see https://stackoverflow.com/questions/2699788/java-is-there-a-subclassof-like-instanceof
+		if(object instanceof TradeContactType && object.getClass()!=TradeContactType.class) {
+			// object is instance of a subclass of TradeContactType, but not TradeContactType itself
+			return (TradeContact)object;
+		} else {
+			return new TradeContact(object); 
+		}
+	}
+
+	// copy ctor
+	private TradeContact(TradeContactType contact) {
+		super();
+		if(contact!=null) {
+			CopyCtor.invokeCopy(this, contact);
+		}
+	}
+
+	/**
+	 * ctor for Seller or Buyer Contact
+	 * 
+	 * @param contactName
+	 * @param contactTel
+	 * @param contactMail
+	 */
+	private TradeContact(String contactName, String contactTel, String contactMail) {
+		setContactPoint(contactName);
+		setContactTelephone(contactTel);
+		setContactEmail(contactMail);
+	}
+	
+	private void setContactPoint(String name) {
+		super.setPersonName(Text.create(name));
+	}
+
+	private void setContactTelephone(String contactTel) {
+		UniversalCommunicationType telephone = new UniversalCommunicationType();
+		telephone.setCompleteNumber(Text.create(contactTel));
+		super.setTelephoneUniversalCommunication(telephone);
+	}
+
+	public void setContactEmail(String contactMail) {
+		UniversalCommunicationType electronicMail = new UniversalCommunicationType();
+		electronicMail.setURIID(new ID(contactMail));
+		super.setEmailURIUniversalCommunication(electronicMail);
+	}
+	
+	@Override
+	public String getContactPoint() {
+		return super.getPersonName().getValue();
+	}
+	
+	@Override
+	public String getContactTelephone() {
+		return super.getTelephoneUniversalCommunication().getCompleteNumber().getValue();
+	}
+	
+	@Override
+	public String getContactEmail() {
+		return super.getEmailURIUniversalCommunication().getURIID().getValue();
+	}
+	
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("[");
+		stringBuilder.append(getContactPoint()==null ? "null" : getContactPoint());
+		stringBuilder.append(", ");
+		stringBuilder.append(getContactTelephone()==null ? "null" : getContactTelephone());
+		stringBuilder.append(", ");
+		stringBuilder.append(getContactEmail()==null ? "null" : getContactEmail());
+		stringBuilder.append("]");
+		return stringBuilder.toString();
+	}
+
+}
