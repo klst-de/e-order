733535d6f42ec43325c0857a5fca9f4eec4d3cf4
homebeaver <homebeaver@outlook.de>
2021-03-04 17:07:53 +0100

    interface for Amount

---
+++ src/main/java/com/klst/edoc/api/IAmount.java
@@ -0,0 +1,38 @@
+package com.klst.edoc.api;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/* Mit diesem Datentyp wird ein Betrag in numerischer Form abgebildet. 
+ * 
+ * Er basiert auf dem Typ „Amount. Type“, wie in ISO 15000-5:2014 Anhang B definiert.
+ * 
+ * Hinweis: Dem Geldwert wird über ein eigenständiges Informationselement eine Währung zugeordnet.
+ * 
+ */
+/**
+ * Amount
+ * 
+ * This is a "decimal" type with 2 digits maximum after the decimal point, without a thousand separator, and with the ". " as a decimal separator. 
+ * It can be supplemented by a "Currency" attribute, if different from the currency in the header. Example 10000.34
+ * 
+ * @see European standard EN 16931-1:2017 : 6.5.2 Amount. Type
+ * 
+ * An amount states a numerical monetary value. 
+ * The currency of the amount is defined as a separate business term. 
+ * 
+ * This EN 16931_ Amount. Type4 is based on the Amount. Type as defined in ISO 15000-5:2014, Annex B. EN 16931_ Amount. 
+ * Type is floating up to two fraction digits.
+ *
+ */
+public interface IAmount extends IAmountFactory, Rounding {
+
+	public BigDecimal getValue(RoundingMode roundingMode);
+
+	// zur Info:
+//	aus IAmountFactory:
+//	public IAmount createAmount(String currencyID, BigDecimal amount);
+//	aus Rounding:
+//	public BigDecimal getValue(RoundingMode roundingMode);
+
+}
---
+++ src/main/java/com/klst/edoc/api/IAmountFactory.java
@@ -0,0 +1,25 @@
+package com.klst.edoc.api;
+
+import java.math.BigDecimal;
+
+/**
+ * abstract-factory, aka Kit for IAmount
+ * 
+ * @see <a href="https://java-design-patterns.com/patterns/abstract-factory/">java-design-patterns</a> for patterns abstract-factory
+ */
+public interface IAmountFactory {
+	
+	/**
+	 * monatary Amount (used in BT-106/Sum of Invoice line net amount or BT-146/Unit price amount f.i.)
+	 * 
+	 * @param currencyID - the currency of the amount is defined as a separate business term. 
+	 * @param amount - a "decimal" type (with 2 or 4 digits after the decimal point f.i.)
+	 * 
+	 * @return Amount object
+	 * 
+	 * @see Amount
+	 * @see UnitPriceAmount
+	 */
+	public IAmount createAmount(String currencyID, BigDecimal amount);
+	
+}
