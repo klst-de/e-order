dc7873bd44e1f6a3d16901b408e22bee05ca44a3
homebeaver <homebeaver@outlook.de>
2021-05-21 15:23:23 +0200

    javadoc

--- src/main/java/com/klst/eorder/api/OrderNoteFactory.java
+++ src/main/java/com/klst/eorder/api/OrderNoteFactory.java
@@ -11,7 +11,7 @@ public interface OrderNoteFactory {
 	 * factory for document Note BG-1 or line Note BG-25.BT-127
 	 * 
 	 * @param content  
-	 * @param optional subjectCode to be chosen from the entries in 
+	 * @param subjectCode - optional, to be chosen from the entries in 
 	 * <a href="https://unece.org/fileadmin/DAM/trade/untdid/d16b/tred/tred4451.htm">UNTDID 4451</a>
 	 * 
 	 * @return NOTE Object
