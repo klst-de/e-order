a16c16cff6d9645f914c647fd8c4522620ea9b8d
homebeaver <homebeaver@outlook.de>
2021-05-23 13:57:45 +0200

    SupplyChainEvent

---
+++ src/main/java/com/klst/eorder/impl/SupplyChainEvent.java
@@ -0,0 +1,27 @@
+package com.klst.eorder.impl;
+
+import com.klst.ebXml.reflection.SCopyCtor;
+
+import un.unece.uncefact.data.standard.reusableaggregatebusinessinformationentity._128.SupplyChainEventType;
+
+public class SupplyChainEvent extends SupplyChainEventType {
+
+	// copy factory
+	static SupplyChainEvent create(SupplyChainEventType object) {
+		if(object instanceof SupplyChainEventType && object.getClass()!=SupplyChainEventType.class) {
+			// object is instance of a subclass of SupplyChainEventType, but not SupplyChainEventType itself
+			return (SupplyChainEvent)object;
+		} else {
+			return new SupplyChainEvent(object); 
+		}
+	}
+
+	// copy ctor
+	private SupplyChainEvent(SupplyChainEventType object) {
+		super();
+		if(object!=null) {
+			SCopyCtor.getInstance().invokeCopy(this, object);
+		}
+	}
+
+}
