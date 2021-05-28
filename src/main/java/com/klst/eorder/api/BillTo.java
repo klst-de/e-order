5f95ddf2497090202e210baee03a3d47891495ce
homebeaver <homebeaver@outlook.de>
2021-05-28 15:33:23 +0200

    "BILL TO" INVOICEE PARTY + INVOICER

---
+++ src/main/java/com/klst/eorder/api/BillTo.java
@@ -0,0 +1,21 @@
+package com.klst.eorder.api;
+
+import com.klst.edoc.api.BusinessParty;
+import com.klst.edoc.api.ContactInfo;
+import com.klst.edoc.api.PostalAddress;
+
+/**
+ * INVOICEE PARTY / The "BILL TO"
+ * <p>
+ * The Party to which the invoice must be sent
+ * <p>
+ * Cardinality: 	0..1 (COMFORT)
+ * <br>Rule ID: 	
+ * <br>Order-X-No: 	833
+ */
+public interface BillTo {
+
+	public void setBillToParty(String name, PostalAddress address, ContactInfo contact);
+	public void setBillToParty(BusinessParty party);
+
+}
---
+++ src/main/java/com/klst/eorder/api/Invoicer.java
@@ -0,0 +1,22 @@
+package com.klst.eorder.api;
+
+import com.klst.edoc.api.BusinessParty;
+import com.klst.edoc.api.ContactInfo;
+import com.klst.edoc.api.PostalAddress;
+
+/**
+ * INVOICER PARTY
+ * <p>
+ * The Party who should issue or send the invoice on behalf of the supplier
+ * <p>
+ * Cardinality: 	0..1 (EXTENDED)
+ * <br>Rule ID: 	
+ * <br>Order-X-No: 	792
+ */
+public interface Invoicer {
+
+	public void setInvoicerParty(String name, PostalAddress address, ContactInfo contact);
+	public void setInvoicerParty(BusinessParty party);
+	public BusinessParty getInvoicerParty();
+
+}
